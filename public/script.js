6fac5da444c8277588c3d05b1f7dd80c25481515
Jonatan <Jonatan.goldbach@gmx.de>
2025-08-05 15:56:17 +0200

    delete function now works in basic

--- public/script.js
+++ public/script.js
@@ -1,210 +1,259 @@
+const testButton = document.getElementById("deleteRoom");
 const chatInput = document.getElementById("chat-box");
-const usernameInput = document.getElementById("username-input")
-const dialog = document.getElementById("dialog")
-const addNewRoomBtnContainer = document.getElementById("add-new-room-btn-container")
-const addNewRoomBtn = document.getElementById("add-new-room-btn")
-const roomsDiv = document.getElementsByClassName("rooms-div")[0]
-const rooms = roomsDiv.querySelectorAll("*")
-let messagesContainer = document.getElementById("messages-container")
-const usersContainer = document.getElementById("users-container")
+const usernameInput = document.getElementById("username-input");
+const dialog = document.getElementById("dialog");
+const addNewRoomBtnContainer = document.getElementById(
+  "add-new-room-btn-container"
+);
+const addNewRoomBtn = document.getElementById("add-new-room-btn");
+const roomsDiv = document.getElementsByClassName("rooms-div")[0];
+const rooms = roomsDiv.querySelectorAll("*");
+let messagesContainer = document.getElementById("messages-container");
+const usersContainer = document.getElementById("users-container");
 const ws = new WebSocket(`ws://${window.location.hostname}:8080/`);
-setActiveRoom(rooms[0])
+setActiveRoom(rooms[0]);
 
-chatInput.value = ""
-usernameInput.value = ""
-dialog.showModal()
+chatInput.value = "";
+usernameInput.value = "";
+dialog.showModal();
 
 ws.onopen = () => {
-  console.log("connected")
-  // ws.send("Hello from the client")
+  console.log("connected");
   rooms.forEach((room) => {
     room.addEventListener("click", () => {
-      ws.send(JSON.stringify({ type: "roomChange", room: room.textContent }))
-      messagesContainer.innerHTML = ""
-      setActiveRoom(room)
-    })
-  })
-}
-
+      ws.send(JSON.stringify({ type: "roomChange", room: room.textContent }));
+      messagesContainer.innerHTML = "";
+      setActiveRoom(room);
+    });
+  });
+};
 
 ws.onmessage = (event) => {
-  console.log("message from server: ", JSON.parse(event.data))
-  const data = JSON.parse(event.data)
+  console.log("message from server: ", JSON.parse(event.data));
+  const data = JSON.parse(event.data);
 
   if (data.type == "alreadyExists") {
-    const warningElem = document.getElementById("already-exists-warning")
+    const warningElem = document.getElementById("already-exists-warning");
     if (warningElem) {
-      return
+      return;
     }
-    const div = document.createElement("div")
-    div.id = "already-exists-warning"
-    div.textContent = "user already exists. Please try another username"
+    const div = document.createElement("div");
+    div.id = "already-exists-warning";
+    div.textContent = "user already exists. Please try another username";
     div.style.color = "red";
-    dialog.appendChild(div)
+    dialog.appendChild(div);
   }
-
 
   if (data.type == "loginSuccess") {
-    dialog.close()
+    dialog.close();
   }
 
-
   if (data.type == "clientList") {
-    usersContainer.innerHTML = ""
+    usersContainer.innerHTML = "";
     data.clients.forEach((client) => {
-      const div = document.createElement("div")
-      div.textContent = client
-      div.classList.add("client")
-      usersContainer.append(div)
-
-    })
+      const div = document.createElement("div");
+      div.textContent = client;
+      div.classList.add("client");
+      usersContainer.append(div);
+    });
   }
 
   if (data.type == "chatMessage") {
     if (getActiveRoom().innerHTML == data.room) {
-      const message = document.createElement("div")
-      message.textContent = `${data.username}: ` + `${data.message}`
-      messagesContainer.appendChild(message)
+      const message = document.createElement("div");
+      message.textContent = `${data.username}: ` + `${data.message}`;
+      messagesContainer.appendChild(message);
     }
   }
 
   if (data.type == "chatHistory") {
     for (const message of data.history[getActiveRoom().innerHTML].messages) {
-      const div = document.createElement('div')
-      div.textContent = `${message.username}: ` + `${message.message}`
-      messagesContainer.appendChild(div)
+      const div = document.createElement("div");
+      div.textContent = `${message.username}: ` + `${message.message}`;
+      messagesContainer.appendChild(div);
     }
   }
 
   if (data.type == "loadRooms") {
-    const roomnames = []
+    const roomnames = [];
     for (const [key, value] of Object.entries(rooms)) {
-      roomnames.push(value.innerHTML)
+      roomnames.push(value.innerHTML);
     }
     for (const room of Object.entries(data.history)) {
-      const roomname = room[0]
+      const roomname = room[0];
       if (roomnames.includes(roomname)) {
-        continue
+        continue;
       }
-      appendRoom(roomname)
+      appendRoom(roomname);
+    }
+  }
+
+  if (data.type == "deleteRoom") {
+    const prevActiveRoom = getActiveRoom();
+    roomsDiv.innerHTML = "";
+    const roomnames = [];
+    for (const [key, value] of Object.entries(data.history)) {
+      roomnames.push(key);
+    }
+    console.log(roomnames)
+    roomnames.forEach((room) => { appendRoom(room) })
+    const prevActiveRoomElem = document.getElementById(`room-${prevActiveRoom.innerHTML}`)
+    if (prevActiveRoomElem) {
+      setActiveRoom(prevActiveRoomElem);
+    } else {
+      setActiveRoom(document.getElementById(`room-general`))
+      loadChat(data.history[getActiveRoom().innerHTML].messages)
     }
   }
 
   if (data.type == "addNewRoom") {
-    appendRoom(data.room)
+    appendRoom(data.room);
   }
 
   if (data.type == "roomChange") {
-    for (const message of data.history[getActiveRoom().innerHTML].messages) {
-      const div = document.createElement('div')
-      div.textContent = `${message.username}: ` + `${message.message}`
-      messagesContainer.appendChild(div)
-    }
+    loadChat(data.history[getActiveRoom().innerHTML].messages)
+    // for (const message of data.history[getActiveRoom().innerHTML].messages) {
+    //   const div = document.createElement("div");
+    //   div.textContent = `${message.username}: ` + `${message.message}`;
+    //   messagesContainer.appendChild(div);
+    // }
   }
 
   if (data.type == "userTyping" && data.room == getActiveRoom().innerHTML) {
-    const typingElem = document.querySelector("#user-typing")
+    const typingElem = document.querySelector("#user-typing");
     if (typingElem) {
-      typingElem.remove()
+      typingElem.remove();
     }
     if (data.userTypingBool) {
-      const div = document.createElement('div')
-      div.id = "user-typing"
-      div.textContent = `${data.username} is typing...`
-      div.style.color = "grey"
-      messagesContainer.appendChild(div)
-      return
+      const div = document.createElement("div");
+      div.id = "user-typing";
+      div.textContent = `${data.username} is typing...`;
+      div.style.color = "grey";
+      messagesContainer.appendChild(div);
+      return;
     }
-
   }
+};
 
-}
-
-
-usernameInput.addEventListener('keydown', (e) => {
-  if (e.key == 'Enter') {
-    ws.username = usernameInput.value
-    ws.send(JSON.stringify({ type: "usernameInput", username: usernameInput.value }))
+usernameInput.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {
+    ws.username = usernameInput.value;
+    ws.send(
+      JSON.stringify({ type: "usernameInput", username: usernameInput.value })
+    );
   }
-})
+});
 
 let typingController = { cancelled: false };
 
 function delay(t) {
-  return new Promise(resolve => setTimeout(resolve, t));
+  return new Promise((resolve) => setTimeout(resolve, t));
 }
 
-chatInput.addEventListener('keydown', async (e) => {
+chatInput.addEventListener("keydown", async (e) => {
   const allowedKeys = /^[a-zA-Z0-9äöüÄÖÜ\-#+!"§\$%&\/()=\?`.:;,<>^°]$/;
-  if (e.key == 'Enter') {
-    ws.send(JSON.stringify({ type: "chatMessage", room: getActiveRoom().innerHTML, username: ws.username, message: chatInput.value }))
-    ws.send(JSON.stringify({ type: "notTyping", room: getActiveRoom().innerHTML }))
-    chatInput.value = ""
-    return
+  if (e.key == "Enter") {
+    ws.send(
+      JSON.stringify({
+        type: "chatMessage",
+        room: getActiveRoom().innerHTML,
+        username: ws.username,
+        message: chatInput.value,
+      })
+    );
+    ws.send(
+      JSON.stringify({ type: "notTyping", room: getActiveRoom().innerHTML })
+    );
+    chatInput.value = "";
+    return;
   }
-  if (e.key == 'Backspace') {
-    ws.send(JSON.stringify({ type: "notTyping", room: getActiveRoom().innerHTML }))
-    return
+  if (e.key == "Backspace") {
+    ws.send(
+      JSON.stringify({ type: "notTyping", room: getActiveRoom().innerHTML })
+    );
+    return;
   }
 
   if (allowedKeys.test(e.key)) {
-    ws.send(JSON.stringify({ type: "userTyping", username: ws.username, room: getActiveRoom().innerHTML }))
+    ws.send(
+      JSON.stringify({
+        type: "userTyping",
+        username: ws.username,
+        room: getActiveRoom().innerHTML,
+      })
+    );
 
-    typingController.cancelled = true
-    const controller = { cancelled: false }
-    typingController = controller
-    await delay(1500)
+    typingController.cancelled = true;
+    const controller = { cancelled: false };
+    typingController = controller;
+    await delay(1500);
 
     if (!controller.cancelled) {
-      ws.send(JSON.stringify({
-        type: "notTyping",
-        username: ws.username,
-        room: getActiveRoom().innerHTML
-      }));
+      ws.send(
+        JSON.stringify({
+          type: "notTyping",
+          username: ws.username,
+          room: getActiveRoom().innerHTML,
+        })
+      );
     }
-    return
+    return;
   }
-  return
-})
+  return;
+});
 
 function addNewRoom() {
-  addNewRoomBtnContainer.innerHTML = ""
-  const newRoomInput = document.createElement('input')
-  newRoomInput.setAttribute("placeholder", "input roomname")
-  newRoomInput.addEventListener('keydown', (e) => {
-    if (e.key == 'Enter') {
-      ws.send(JSON.stringify({ type: "addNewRoom", room: newRoomInput.value }))
+  addNewRoomBtnContainer.innerHTML = "";
+  const newRoomInput = document.createElement("input");
+  newRoomInput.setAttribute("placeholder", "input roomname");
+  newRoomInput.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+      ws.send(JSON.stringify({ type: "addNewRoom", room: newRoomInput.value }));
     }
-  })
-  addNewRoomBtnContainer.append(newRoomInput)
+  });
+  addNewRoomBtnContainer.append(newRoomInput);
 }
 
-addNewRoomBtn.addEventListener('click', () => {
-  addNewRoom()
-})
+addNewRoomBtn.addEventListener("click", () => {
+  addNewRoom();
+});
 
 function appendRoom(roomname) {
-  const newRoomBtn = document.createElement('button')
-  newRoomBtn.classList.add("room-btn")
-  newRoomBtn.innerHTML = roomname
+  const newRoomBtn = document.createElement("button");
+  newRoomBtn.classList.add("room-btn");
+  newRoomBtn.innerHTML = roomname;
+  newRoomBtn.id = `room-${roomname}`
   newRoomBtn.addEventListener("click", () => {
-    ws.send(JSON.stringify({ type: "roomChange", room: newRoomBtn.textContent }))
-    messagesContainer.innerHTML = ""
-    setActiveRoom(newRoomBtn)
-  })
-  roomsDiv.append(newRoomBtn)
-  addNewRoomBtnContainer.innerHTML = ""
-  addNewRoomBtnContainer.appendChild(addNewRoomBtn)
+    ws.send(
+      JSON.stringify({ type: "roomChange", room: newRoomBtn.textContent })
+    );
+    messagesContainer.innerHTML = "";
+    setActiveRoom(newRoomBtn);
+  });
+  roomsDiv.append(newRoomBtn);
+  addNewRoomBtnContainer.innerHTML = "";
+  addNewRoomBtnContainer.appendChild(addNewRoomBtn);
 }
 
 function getActiveRoom() {
-  return document.querySelector('.active-room')
+  return document.querySelector(".active-room");
 }
 
 function setActiveRoom(newActiveRoom) {
   if (getActiveRoom()) {
-    getActiveRoom().classList.remove("active-room")
+    getActiveRoom().classList.remove("active-room");
   }
-  newActiveRoom.classList.add("active-room")
+  newActiveRoom.classList.add("active-room");
 }
 
+function loadChat(chatMessages) {
+  for (const message of chatMessages) {
+    const div = document.createElement("div");
+    div.textContent = `${message.username}: ` + `${message.message}`;
+    messagesContainer.appendChild(div);
+  }
+}
+
+function deleteRoom(room) {
+  ws.send(JSON.stringify({ type: "deleteRoom", room: room }));
+}
--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-import express from 'express';
-import { WebSocketServer } from 'ws';
-import { readFileSync, writeFile } from 'node:fs';
+import express from "express";
+import { WebSocketServer } from "ws";
+import { readFileSync, writeFile } from "node:fs";
 const app = express();
 const port = 8080;
 
+app.use(express.static("public"));
 
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-  res.sendFile('/home/jonatan/Projects/chat/index.html'); // serve your HTML file
+app.get("/", (req, res) => {
+  res.sendFile("/home/jonatan/Projects/chat/index.html"); // serve your HTML file
 });
-
 
 const server = app.listen(port, () => {
   console.log(`HTTP server listening on http://localhost:${port}`);
@@ -20,108 +18,161 @@ const wss = new WebSocketServer({ server }); // use the same server for WS
 
 wss.getUniqueID = function() {
   function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
   }
-  return s4() + s4() + '-' + s4();
+  return s4() + s4() + "-" + s4();
 };
 
 function loadChatHistory() {
-  const data = readFileSync("./data/ChatHistory.json", 'utf8')
-  return JSON.parse(data)
+  const data = readFileSync("./data/ChatHistory.json", "utf8");
+  return JSON.parse(data);
 }
 
 function writeChatHistory(data) {
-  const parsedData = JSON.stringify(data)
-  writeFile("./data/ChatHistory.json", parsedData, 'utf8', function(err) { if (err) { console.error(err) } console.log("file written succesfully") })
+  const parsedData = JSON.stringify(data);
+  writeFile("./data/ChatHistory.json", parsedData, "utf8", function(err) {
+    if (err) {
+      console.error(err);
+    }
+    console.log("file written succesfully");
+  });
 }
 
+let chatHistory = loadChatHistory();
 
-let chatHistory = loadChatHistory()
+let clientList = new Set();
+console.log("clientList", clientList);
 
-let clientList = new Set()
-console.log("clientList", clientList)
-
-wss.on('connection', (ws) => {
-  ws.on('error', console.error);
+wss.on("connection", (ws) => {
+  ws.on("error", console.error);
   ws.id = wss.getUniqueID();
 
-  ws.send(JSON.stringify({ type: "clientList", clients: Array.from(clientList) }))
-  ws.send(JSON.stringify({ type: "chatHistory", history: chatHistory }))
-  ws.send(JSON.stringify({ type: "loadRooms", history: chatHistory }))
+  ws.send(
+    JSON.stringify({ type: "clientList", clients: Array.from(clientList) })
+  );
+  ws.send(JSON.stringify({ type: "chatHistory", history: chatHistory }));
+  ws.send(JSON.stringify({ type: "loadRooms", history: chatHistory }));
 
-
-  ws.on('message', function message(data) {
-    const dataMessage = JSON.parse(data)
+  ws.on("message", function message(data) {
+    const dataMessage = JSON.parse(data);
 
     if (dataMessage.type == "usernameInput") {
       if (clientList.has(dataMessage.username)) {
-        ws.send(JSON.stringify({ type: "alreadyExists" }))
-        return
+        ws.send(JSON.stringify({ type: "alreadyExists" }));
+        return;
       }
-      ws.send(JSON.stringify({ type: "loginSuccess" }))
-      ws.username = dataMessage.username
-      clientList.add(dataMessage.username)
-      console.log("clientList", clientList)
+      ws.send(JSON.stringify({ type: "loginSuccess" }));
+      ws.username = dataMessage.username;
+      clientList.add(dataMessage.username);
+      console.log("clientList", clientList);
       wss.clients.forEach(function each(client) {
         if (client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify({ type: "clientList", clients: Array.from(clientList) }))
+          client.send(
+            JSON.stringify({
+              type: "clientList",
+              clients: Array.from(clientList),
+            })
+          );
         }
-      })
+      });
     }
 
-    console.log("data got ", dataMessage)
+    console.log("data got ", dataMessage);
 
     if (dataMessage.type == "chatMessage") {
-      chatHistory[dataMessage.room].messages.push({ username: ws.username, message: dataMessage.message })
-      writeChatHistory(chatHistory)
+      chatHistory[dataMessage.room].messages.push({
+        username: ws.username,
+        message: dataMessage.message,
+      });
+      writeChatHistory(chatHistory);
       wss.clients.forEach(function each(client) {
         if (client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify({ type: "chatMessage", room: dataMessage.room, username: ws.username, message: dataMessage.message }))
+          client.send(
+            JSON.stringify({
+              type: "chatMessage",
+              room: dataMessage.room,
+              username: ws.username,
+              message: dataMessage.message,
+            })
+          );
         }
-      })
+      });
     }
 
     if (dataMessage.type == "roomChange") {
-      ws.send(JSON.stringify({ type: "roomChange", history: chatHistory }))
+      ws.send(JSON.stringify({ type: "roomChange", history: chatHistory }));
     }
 
     if (dataMessage.type == "addNewRoom") {
-      chatHistory[dataMessage.room] = { messages: [] }
+      chatHistory[dataMessage.room] = { messages: [] };
       wss.clients.forEach(function each(client) {
         if (client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify({ type: "addNewRoom", room: dataMessage.room }))
+          client.send(
+            JSON.stringify({ type: "addNewRoom", room: dataMessage.room })
+          );
         }
-      })
+      });
     }
 
     if (dataMessage.type == "userTyping") {
       wss.clients.forEach(function each(client) {
         if (client !== ws && client.readyState == WebSocket.OPEN) {
-          client.send(JSON.stringify({ type: "userTyping", username: dataMessage.username, userTypingBool: true, room: dataMessage.room }))
+          client.send(
+            JSON.stringify({
+              type: "userTyping",
+              username: dataMessage.username,
+              userTypingBool: true,
+              room: dataMessage.room,
+            })
+          );
         }
-      })
+      });
     }
 
     if (dataMessage.type == "notTyping") {
       wss.clients.forEach(function each(client) {
         if (client !== ws && client.readyState == WebSocket.OPEN) {
-          client.send(JSON.stringify({ type: "userTyping", room: dataMessage.room, userTypingBool: false }))
+          client.send(
+            JSON.stringify({
+              type: "userTyping",
+              room: dataMessage.room,
+              userTypingBool: false,
+            })
+          );
         }
-      })
+      });
+    }
+
+    if (dataMessage.type == "deleteRoom") {
+      console.log("test", chatHistory);
+      delete chatHistory[dataMessage.room];
+      console.log("what", dataMessage, chatHistory);
+      wss.clients.forEach(function each(client) {
+        if (client.readyState == WebSocket.OPEN) {
+          client.send(
+            JSON.stringify({ type: "deleteRoom", history: chatHistory })
+          );
+        }
+      });
     }
 
     ws.on("close", () => {
-      clientList.delete(ws.username)
-      console.log("clientList", clientList)
+      clientList.delete(ws.username);
+      console.log("clientList", clientList);
       wss.clients.forEach(function each(client) {
         if (client !== ws && client.readyState == WebSocket.OPEN) {
-          client.send(JSON.stringify({ type: "clientList", clients: Array.from(clientList) }))
+          client.send(
+            JSON.stringify({
+              type: "clientList",
+              clients: Array.from(clientList),
+            })
+          );
         }
-      })
-    })
-
+      });
+    });
   });
 
-  console.log('Client connected');
-})
-
+  console.log("Client connected");
+});
